59856f0634d361c6765fd5ad7440aaf20454796e
David Oakley <01dboakley@gmail.com>
2020-02-16 14:01:35 -0500

    Week 3 Cookies

--- app.js
+++ app.js
@@ -37,33 +37,54 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321')); // cookie key
+
 // authentication
 function auth(req, res, next) {
   console.log('**REQUEST HEADER', req.headers);
-  const authHeader = req.headers.authorization;
-  // if authHeader is null
-  if (!authHeader) {
-      const err = new Error('You are NOT authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401; // unauthorized code
-      return next(err); // pass error to express
-  }
-  // parse user name and password to the auth array
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  console.log('**AUTH HEADER is ', authHeader);
-  console.log('**AUTH is ',auth);
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-      return next(); // authorized, access is granted
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    // if authHeader is null
+    if (!authHeader) {
+        const err = new Error('You are NOT authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401; // unauthorized code
+        return next(err); // pass error to express
+    }
+    // parse user name and password to the auth array
+    
+    const auth1 = Buffer.from(authHeader.split(' ')[1], 'base64');
+    console.log('AUTH1 is ', auth1);
+    let auth2 = auth1.toString();
+    console.log('AUTH2 is', auth2);
+    let auth3 = auth2.split(':');
+    console.log('AUTH3 is ',auth3);
+
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    console.log('**AUTH HEADER is ', authHeader);
+    console.log('**AUTH is ',auth);
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+        res.cookie('user', 'admin', {signed: true}); // create cookie  
+        return next(); // authorized, access is granted
+    } else {
+        const err = new Error('You are so not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        return next(err);
+      }
   } else {
-      const err = new Error('You are so not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      return next(err);
-  }
-}
+    if (req.signedCookies.user === 'admin') {
+        return next(); // access granted
+    } else {
+        const err = new Error('Wait, you are not authenticated!');
+        err.status = 401;
+        return next(err);
+      }
+    }
+} // end auth function
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
